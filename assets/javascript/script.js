0a992645f553263c987894e68b49fcaae07f58b1
Ericcentrik <enaiman1@gmail.com>
2019-01-15 23:31:20 -0500

    new html,css, js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,10 +1,4 @@
+// script for scroll event plugin
 
-//-23% split1
-
-//-37% split2
-console.log($("#split1"));
-
-$("#split1").animate({"left":"-23%"}, 2000);
-$("#split2").animate({"right":"-37%"}, 2000, function() {
-    $("#split-background").fadeIn();
-});
+$(document).animateScroll(); 
+//
